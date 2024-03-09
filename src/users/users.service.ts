182a0fbc0943ac427a69f7e6e59c08cbe4174a7e
Hal-ang <jpjgv05@gmail.com>
2024-03-09 14:04:51 +0900

    Implement data-source.ts for migration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,15 @@
-import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
-  ) {}
-
-  findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+  getUser(fcmToken: string): Promise<User | null> {
+    return null;
   }
 
-  findOne(id: number): Promise<User | null> {
-    return this.usersRepository.findOneBy({ id });
-  }
-
-  async remove(id: number): Promise<void> {
-    await this.usersRepository.delete(id);
+  postUser(fcmToken: string): Promise<User | null> {
+    return null;
   }
 }
