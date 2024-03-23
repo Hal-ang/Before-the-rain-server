d299260e2c3be0737db1e57e2fad4d01c038d697
Hal-ang <jpjgv05@gmail.com>
2024-03-23 18:08:26 +0900

    Implement sendSlientPush

--- src/alert/messaging.service.ts
+++ src/alert/messaging.service.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 
 import { Injectable } from '@nestjs/common';
-import { TIME_PERIODS } from '../surveys/constants';
 
 @Injectable()
 export class MessagingService {
@@ -25,7 +24,34 @@ export class MessagingService {
       const response = await admin.messaging().send(message);
       console.log('Successfully sent message:', response);
     } catch (e) {
-      throw new Error(e);
+      console.log(e);
+    }
+  }
+
+  async sendSilentPush(fcmToken: string) {
+    const message = {
+      apns: {
+        headers: {
+          'apns-priority': '5',
+          'apns-push-type': 'background',
+        },
+        payload: {
+          aps: {
+            'content-available': 1,
+            badge: 1,
+            sound: 'default',
+          },
+        },
+      },
+      token: fcmToken,
+    };
+    // FCM 메시지 보내기
+    try {
+      const response = await admin.messaging().send(message);
+      console.log('Successfully sent slient message:', response);
+    } catch (e) {
+      // 에러 발생 시 유저 정보 삭제 고려하기
+      console.log(e);
     }
   }
 }
